5ba2443e933c12176b7a6274c5c9e92349a5a674
Carlo Elvisi <carloelvisi01@outlook.it>
2024-12-03 15:42:04 +0100

    form permette di creare lista nomi

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,36 @@
 import React, { useState } from "react";
 
 function App() {
-  const [firstName, setFirstName] = useState("Marco");
+  const [firstName, setFirstName] = useState("Carlo");
+  const [names, setNames] = useState([]);
+
+  const addName = (e) => {
+    e.preventDefault();
+    if (firstName !== "") {
+      setNames([...names, firstName]); // Aggiunge il nome alla lista
+      setFirstName(""); // Resetta l'input
+    }
+  };
 
   return (
     <>
-      <form>
+      <form onSubmit={addName}>
         <input
           type="text"
           value={firstName}
           onChange={(e) => setFirstName(e.target.value)}
+          placeholder="Inserisci un nome"
         />
-        <p>Il tuo nome è {firstName}</p>
+        <button type="submit">Aggiungi Nome</button>
       </form>
+      <p>Il tuo nome è {firstName}</p>
+
+      {/* Lista dei nomi */}
+      <ul>
+        {names.map((name, index) => (
+          <li key={index}>{name}</li>
+        ))}
+      </ul>
     </>
   );
 }
