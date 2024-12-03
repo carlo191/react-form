861a6eb19ee976b15d4366b4c3844c398d7969ef
Carlo Elvisi <168591977+carlo191@users.noreply.github.com>
2024-12-03 15:16:34 +0100

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+// import { useState } from 'react'
+
+function App() {
+  return (
+    <>
+      <div className="container">
+        <h1 className="mt-5">Hello World</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
